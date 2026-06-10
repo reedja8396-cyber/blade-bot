c31b38d92ec6c1906c58c88416186dc4e02e02b3
rpglover <reedja8396@gmail.com>
2026-06-10 18:46:36 -0400

    Update owner.py

--- owner.py
+++ owner.py
@@ -11,7 +11,5 @@ class Developer(commands.Cog):
     async def memory(self, ctx): 
         await ctx.send(f"RAM Usage: {psutil.virtual_memory().percent}%") 
 
-# ◄— Make sure you deleted the extra @commands.command() lines at the bottom!
-
 async def setup(bot):
     await bot.add_cog(Developer(bot))
